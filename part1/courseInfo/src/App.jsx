6b603d6379522a41c885f3cd34d6a5c4cdd57046
bustedelbow <velozity2@gmail.com>
2024-03-09 23:42:48 +0800

    Finished exercises 1.4

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -29,18 +29,20 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half stack application development'
-  const part1 = {
-    name: 'Fundamentals of Reach',
-    exercises: 10,
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  }
-  const part3 = {
-    name: 'State of component',
-    exercises: 14,
-  }
+  const part = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of component',
+      exercises: 14,
+    }
+  ]
   
   return (
     <>
